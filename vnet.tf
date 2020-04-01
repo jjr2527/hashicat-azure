29807fa2ec30c99b633aa0497d49da987060776c
root <root@p-4ojwgqhghox9-workstation.c.instruqt-prod.internal>
2020-04-01 20:21:03 +0000

    Jason is dumb

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/rendel/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "rendel-workspace"
+  resource_group_name = "rendel-workshop"
 }
