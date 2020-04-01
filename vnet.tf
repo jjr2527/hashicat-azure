9594bd5675a960329733ebe4149c8da2348ccc50
root <root@p-4ojwgqhghox9-workstation.c.instruqt-prod.internal>
2020-04-01 20:02:30 +0000

    More of a fix

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,6 @@
-resource "azurerm_resource_group" "test" {  
-    name     = "rendel-workshop"  
-    location = "CentralUS"
-}
+module "network" {
+  source  = "app.terraform.io/rendel/network/azurerm"
+  version = "3.0.1"
 
-module "network" {  
-    source  = "app.terraform.io/jjr2527/network/azurerm"  
-    resource_group_name = azurerm_resource_group.test.name  
-    version = "3.0.1"
+  resource_group_name = "rendel-workspace"
 }
